7b9e8cc50b6b3269950ab6ec08319731cff0b9c5
Matthew Macon <matthewmacon90@gmail.com>
2022-12-15 03:57:33 -0500

    Building Car

--- src/features/homeDisplay/DisplayCarousel.js
+++ src/features/homeDisplay/DisplayCarousel.js
@@ -1,35 +1,75 @@
 import { useState } from 'react';
 import {
     Row,
+    Col,
     Carousel,
     CarouselItem,
     CarouselControl,
     CarouselIndicators,
-    CarouselCaption,
+    // CarouselCaption,
   } from 'reactstrap';
   import {selectFeaturedGames} from '../games/gamesSlice';
-  
-  const DisplayCarousel = ({items}) => {
-    const {name, image} = items;
-    const [activeIdx, setActiveIdx] = useState(0);
-    const featuredGames = selectFeaturedGames();
-    return (
-        <Row>{
-            featuredGames.map((game) => {
-                return (
-                    <Carousel>
-                        <CarouselItem>
-                            <img src={image} alt={name}/>
-                        </CarouselItem>
-                        <CarouselIndicators 
-                            items={game}
-                        />
-                    </Carousel>
+  import './carouselStyle.css'
 
-                );
-            })}
+  const DisplayCarousel = () => {
+    const [activeIdx, setActiveIdx] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const featuredGames = selectFeaturedGames();
+
+    const previousButton = () => {
+        if (animating) return;
+        const nextIdx = activeIdx === 0 ?
+        featuredGames.length : activeIdx - 1;
+        setActiveIdx(nextIdx);
+    }
+
+    const nextButton = () => {
+        if (animating) return;
+        const nextIdx = activeIdx === featuredGames.length ?
+        0 : activeIdx + 1;
+        setActiveIdx(nextIdx);
+    }
+
+    const slides = featuredGames.map((game) => {
+        return (
+            <CarouselItem
+                key={game.id}
+                onExited={() => setAnimating(false)}
+                onExiting={() => setAnimating(true)}
+            >
+                <img src={game.image} alt={game.name} />
+            </CarouselItem>
+        );
+    });
+    return (
+        <Row>
+            <Col>
+                <h3>Join Us in these Games!</h3>
+                <Carousel
+                    previous={previousButton}
+                    next={nextButton}
+                    activeIndex={activeIdx}
+                    dark
+                    fade
+                >
+                    <CarouselIndicators 
+                        items={featuredGames}
+                        activeIndex={activeIdx}
+                        onClickHandler={(newIndex) => {
+                            if (animating) return;
+                            setActiveIdx(newIndex);
+                        }} />
+                        {slides}
+                        <CarouselControl directionText="Previous"
+                            direction="prev" onClickHandler={previousButton} />
+                        <CarouselControl directionText="Next"
+                            direction="next" onClickHandler={nextButton} />
+                </Carousel>
+            </Col>
         </Row>
     );
+
 };
 
 
